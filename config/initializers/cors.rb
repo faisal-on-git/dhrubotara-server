3c106d9689bbe1c2c54fa60ef2bc2b10be5fb42c
faisal-work <faisal@rently.com>
2025-02-17 07:22:11 +0530

    added-authentication

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # Vite's default development server port
+    origins '*'  # Later change this to your frontend domain
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      expose: ['Authorization']
   end
 end
